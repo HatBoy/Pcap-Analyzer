f9dd9a90dd2868dc67157d44a3f7bcabd2bbd2db
yuluo <yuluo-ding@users.noreply.github.com>
2019-03-04 11:04:18 +0800

    更新flask_wtf.form的名字。
    根据最新版本建议，建议更新为`from flask_wtf import FlaskForm`

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,15 @@
 #coding:UTF-8
 __author__ = 'dj'
 
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
 from wtforms.validators import DataRequired, AnyOf
 
 #上传表单
-class Upload(Form):
+class Upload(FlaskForm):
     pcap = FileField('pcap', validators=[DataRequired()])
 
 #协议过滤表单
-class ProtoFilter(Form):
+class ProtoFilter(FlaskForm):
     value = FileField('value')
     filter_type = FileField('filter_type', validators=[DataRequired(), AnyOf([u'all', u'proto', u'ipsrc', u'ipdst'])])
